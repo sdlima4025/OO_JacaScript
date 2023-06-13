ba0eea0e2368972ea54524843e0d1ccf9268c144
sdlima4025 <sergiolimafg3@gmail.com>
2023-06-13 17:37:49 -0300

    geter and setrs

--- script.js
+++ script.js
@@ -1,41 +1,69 @@
 // Classe - Template Molde Modelo
  class Person {
     // variavéis Globais
-    age = 0;
-    steps = 0;
+    age = 10;
+    // steps = 0;
 
     constructor(name) {
         this.name = name;
-      
-    }
-    // Ações do contrutor
-    takeAStep() {
-        this.steps++;
-    }
-    setAge(newAge) {
-        if(typeof newAge == 'number') {
-            this.age = newAge;
-        }else {
-            console.log("O campo sóm aceita Números!");
-        }
     }
  }
+// Classes Herança
+    class Stundent extends Person {
+
+        constructor(name, id) {
+            super(name);
+            this.id = id;
+        }
+    }
+
+
+    // constructor(firstName, lastName) {
+    //     this.firstName = firstName;
+    //     this.lastName = lastName;
+    // }
+    // Ações do contrutor
+    // takeAStep() {
+    //     this.steps++;
+    // }
+    // get fullName() {
+    //     return(`${this.firstName} ${this.lastName}`);
+    // }
+    // get age() {
+    //     return this._age;
+    // }
+    // set age(x) {
+    //     if(typeof x === 'number') {
+    //         this._age = x;
+
+    //     }else {
+    //         console.log("O Campo só aceita Números!")
+    //     }
+    // }
+
+    // setAge(newAge) {
+    //     if(typeof newAge == 'number') {
+    //         this.age = newAge;
+    //     }else {
+    //         console.log("O campo sóm aceita Números!");
+    //     }
+    // }
+
 
  // Instâmcia da classe | p1 é a criação de Objeto.
- let p1 = new Person("Sérgio");
- let p2 = new Person("Gilton");
- let p3 = new Person("Reinaldo");
+//  let p1 = new Person("Sérgio", "Lima");
+//  let p2 = new Person("Gilton", "Lima");
+//  let p3 = new Person("Reinaldo", "Lima");
 
-p1.setAge(52);
- console.log(`${p1.name} tem ${p1.age} anos!`);
+//  p1.age = '52';
+//  console.log(`${p2.fullName} tem ${p1.age} anos!`);
 
  
- p1.takeAStep();
- console.log(`Passos de ${p1.name}: ${p1.steps}`);
- console.log(`Passos de ${p2.name}: ${p2.steps}`);
+//  p1.takeAStep();
+//  console.log(`Passos de ${p1.name}: ${p1.steps}`);
+//  console.log(`Passos de ${p2.name}: ${p2.steps}`);
  
 //  p1.age = 52;
 //  console.log(`${p1.name} tem ${p1.age} anos`);
 //  console.log(`${p2.name} tem ${p2.age} anos`);
 //  console.log(`${p3.name} tem ${p3.age} anos`);
-//Objetos
